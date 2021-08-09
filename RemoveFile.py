0b5cf0477d0bd7c960c8463231e7d6eed15cec6e
ishan <scifyfuntime@gmail.com>
2021-08-09 19:08:40 +0530

    It works

--- RemoveFile.py
+++ RemoveFile.py
@@ -2,21 +2,19 @@ import os
 import shutil
 import time
 
-days = time.time()
+days = 20
+seconds = time.time() - (days*24*60*60)
 path = input('Which files to remove:-')
 
 def getTime():
     ctime = os.stat(path).st_ctime
     return ctime
 
-
 getTime()
 
 os.path.exists(path)
-list_of_files = os.walk(path)
-os.path.join(path)
 
-if getTime() > days :
+if seconds > days :
     os.remove(path)
 else:
     print('The required path does not exist')
